1fa098d41dc8355185e5b29d64410cf0998c8d3f
elyaskhatinzadeh <124669887+elyaskhatinzadeh@users.noreply.github.com>
2024-06-19 02:23:00 +0330

    Update ModalSection.tsx

--- src/components/ModalSection/ModalSection.tsx
+++ src/components/ModalSection/ModalSection.tsx
@@ -1,5 +1,8 @@
 import styles from './ModalSection.module.css';
 import {Button, Input, List, Modal, Placeholder, Section} from '@xelene/tgui';
+import {ModalHeader} from "@xelene/tgui/dist/components/Overlays/Modal/components/ModalHeader/ModalHeader";
+import {ModalClose} from "@xelene/tgui/dist/components/Overlays/Modal/components/ModalClose/ModalClose";
+import {Icon28Close} from "@xelene/tgui/dist/icons/28/close";
 
 const PlaceholderImage = () => (
     <img
@@ -11,7 +14,8 @@ const PlaceholderImage = () => (
 
 const ModalWithTrigger = () => (
     <Modal
-        header={<Modal.Header />}
+        snapPoints={[1]}
+        header={<ModalHeader after={<ModalClose><Icon28Close style={{color: 'var(--tgui--plain_foreground)'}} /></ModalClose>}>Only iOS header</ModalHeader>}
         trigger={<Button size="l" stretched>Open modal</Button>}
     >
         <List>
